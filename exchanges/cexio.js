465b03e95b1b9898cc1542c67b29fde07e0bcc50
Alexander Zschach <alex@zschach.net>
2013-11-27 00:27:14 +0100

    Ongoing development.  Added NeDB for holding historical data to not bother the cex.io API anymore

--- exchanges/cexio.js
+++ exchanges/cexio.js
@@ -1,11 +1,11 @@
-var cexio = require('cexio');
-
-var moment = require('moment');
-var nstore = require('nstore');
-nstore= nstore.extend(require('nstore/query')());
-var util = require('../util');
-var _ = require('lodash');
-var log = require('../log')
+var cexio= require('cexio'),
+   moment= require('moment'),
+     nedb= require('nedb'),
+    async= require('async'),
+       db= new nedb({filename: 'cexio.db', autoload: true}),
+        _= require('lodash'),
+     util= require('../util'),
+      log= require('../log');
 
 var Trader = function(config) {
   this.key = config.key;
@@ -18,6 +18,120 @@ var Trader = function(config) {
 
   // this.cexio = new cexio(this.key, this.secret);
   this.cexio= new cexio(this.pair);
+
+}
+
+Trader.prototype.getTrades = function(since, callback, descending) {
+  var self= this;
+  var last_tid= next_tid= 0;
+
+  if(since && !_.isNumber(since))
+    since = util.toMicro(since);
+
+  var args = _.toArray(arguments);
+
+  async.waterfall([
+    function(callback) {
+      db.find({}, function(err, docs) {
+        if(!docs || docs.length === 0)
+          tid= 263000;
+        else
+          tid= 1 + _.max(docs, 'tid').tid;
+
+        log.info(self.name, 'Updating cex.io historical data store');
+        log.debug(self.name, 'Fetching from tid ' + tid);
+
+        self.cexio.trades({since: tid},
+          function(err, trades) {
+            if(err || !trades || trades.length === 0)
+              self.retry(self.getTrades, args);
+            else {
+              trades= trades.reverse();
+              _.forEach(trades, function(trade) {
+                // convert to int
+                trade.amount= Number(trade.amount);
+                trade.price= Number(trade.price);
+                trade.tid= Number(trade.tid);
+                trade.date= Number(trade.date);
+                db.insert(trade);
+              });
+            }
+            callback();
+        });
+      });
+    },
+    function(callback) {
+      if(!since) {
+        since= new Date().getTime() * 1000;
+        since-= (10 * 1000 * 1000);
+      }
+      since= Math.floor(since / 1000 / 1000);
+      log.debug('fetching since ' + since);
+
+      db.find({'date': {$gte: since}}, function(err, docs) {
+        docs= _.sortBy(docs, 'tid');
+        // log.debug(self.name, docs);
+        if(!docs || docs.length === 0)
+          self.retry(self.getTrades, args);
+        callback(null, docs);
+      });
+    }
+  ], function(err, result) {
+    if(err) return log.error(self.name, 'error: ' + err);
+    callback(result);
+  });
+
+
+
+  // let the db reflect the latest trades from cex.io
+  // db.find({}, function(err, docs) {
+  //   console.log(_.max(docs, 'tid'));
+  //   if(!docs || docs.length === 0)
+  //     last_tid= 263000;
+  //   else
+  //     last_tid= _.max(docs, 'tid').tid;
+
+  //   next_tid= 1 + last_tid;
+
+  //   var args= _.toArray(arguments);
+  //   log.debug(self.name, 'Fetching from txid ' + next_tid);
+  //   self.cexio.trades({since: next_tid},
+  //     function(err, trades) {
+  //       if(err || !trades || trades.length === 0)
+  //         return self.retry(self.getTrades, args);
+  //       // cex.io deliver them in desc order
+  //       trades= trades.reverse();
+  //       _.forEach(trades, function(trade) {
+  //         // log.debug(self.name, 'storing tid ' + trade.tid);
+  //         db.insert(trade);
+  //       });
+
+  //       db.find({date: {$gt: (since / 1000)}}, function(err, docs)) {
+  //       }
+
+  //       db.find({}, function(err, docs) {});
+  //   }, this);
+  // });
+
+
+  // if(since && !_.isNumber(since))
+  //   since= 263000;
+  // else
+  //   since= this.next_tid;
+
+  // var next_tid= 0;
+  // //console.log('fetching since (incl.) tid:' + since);
+
+  // this.cexio.trades({since: since}, _.bind(function(err, trades) {
+  //   if(err || !trades)
+  //     return this.retry(this.getTrades, args);
+  //   if(trades.length === 0)
+  //     return this.retry(this.getTrades, args);
+  //   // remember, where we are for the next fetch
+  //   this.next_tid= ++(trades[0].tid);
+  //   // cex.io returns descending trade list
+  //   callback(trades.reverse());
+  // }, this));
 }
 
 Trader.prototype.buy = function(amount, price, callback) {
@@ -64,36 +178,25 @@ Trader.prototype.sell = function(amount, price, callback) {
 
 // if cex.io errors we try the same call again after
 // 5 seconds or half a second if there is haste
-Trader.prototype.retry = function(method, callback, haste) {
-  var wait = +moment.duration(haste ? 0.5 : 5, 'seconds');
-  log.debug(this.name , 'returned an error, retrying..');
+Trader.prototype.retry = function(method, args) {
+  var wait = +moment.duration(10, 'seconds');
+  log.debug(this.name, 'returned an error, retrying..');
+
+  var self = this;
+
+  // make sure the callback (and any other fn)
+  // is bound to Trader
+  _.each(args, function(arg, i) {
+    if(_.isFunction(arg))
+      args[i] = _.bind(arg, self);
+  });
+
+  // run the failed method again with the same
+  // arguments after wait
   setTimeout(
-    _.bind(method, this),
-    wait,
-    _.bind(callback, this)
+    function() { method.apply(self, args) },
+    wait
   );
-}
-
-Trader.prototype.getTrades = function(since, callback, descending) {
-  if(since && !_.isNumber(since))
-    since= 263000;
-  else
-    since= this.next_tid;
-
-  var args = _.toArray(arguments);
-  var next_tid= 0;
-  //console.log('fetching since (incl.) tid:' + since);
-
-  this.cexio.trades({since: since}, _.bind(function(err, trades) {
-    if(err || !trades)
-      return this.retry(this.getTrades, args);
-    if(trades.length === 0)
-      return this.retry(this.getTrades, args);
-    // remember, where we are for the next fetch
-    this.next_tid= ++(trades[0].tid);
-    // cex.io returns descending trade list
-    callback(trades.reverse());
-  }, this));
 }
 
 Trader.prototype.getPortfolio = function(callback) {
