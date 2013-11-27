bb31ab0805ec7bf1ca9cd7ea3b83b1344ba2a7d8
Alexander Zschach <alex@zschach.net>
2013-11-27 16:01:58 +0100

    Changed buy/sell threshold

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ config.EMA = {
   // amount of candles to remember and base initial EMAs on
   candles: 100,
   // the difference between the EMAs (to act as triggers)
-  sellTreshold: -0.1,
-  buyTreshold: 0.1
+  sellTreshold: -0.25,
+  buyTreshold: 0.25
 };
 
 // Monitor the live market
